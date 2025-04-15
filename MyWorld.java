1aded792c31cfe7b8aabac2fac324c3067e70149
owenchlee <owenlees888@gmail.com>
2025-04-15 13:57:06 -0400

    Add baby

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero baby = new Hero();
+        addObject(baby, 100, 100);
     }
 }
